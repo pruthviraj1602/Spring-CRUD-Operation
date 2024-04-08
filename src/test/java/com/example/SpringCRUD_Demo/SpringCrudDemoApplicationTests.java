52a173bdef754d43fcb7108ff8ba4efcbce0d08c
Pruthviraj Tanaji Patil <pruthviraj7191@gamil.com>
2024-04-08 12:37:10 +0530

    New crud Opretation

---
+++ src/test/java/com/example/SpringCRUD_Demo/SpringCrudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringCRUD_Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
